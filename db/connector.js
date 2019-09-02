6bb2dd54e47b3f98698894417a93d787b29e52ad
Choih0401 <danielch0401@naver.com>
2019-09-03 08:38:05 +0900

    make db name env

--- db/connector.js
+++ db/connector.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 const db = {
-  host: 'localhost',
+  host: `${process.env.DB_HOST}`,
   port: 3306,
-  user: 'root',
-  password: 'ch04010508',
-  database: '2019iot'
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASS}`,
+  database: `${process.env.DB_NAME}`
 };
 
 var connector
