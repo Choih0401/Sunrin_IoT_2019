1dfe6c07aee63bfacfb8f89eaeb4beef7e291f8a
naruteride <tadanide@gmail.com>
2019-09-12 07:00:25 +0900

    인덱스 페이지 수정

---
+++ public/scripts/index.js
@@ -0,0 +1,27 @@
+let rooms = document.querySelectorAll(".room");    // 컴퓨터실
+let btnOpens = document.querySelectorAll(".btn-open");    // 열기 버튼
+let segyero = document.querySelector("#segyero");    // 세계로
+
+// 컴퓨터실 객체 클릭
+for (let room of rooms) {
+    room.addEventListener("click", function () {
+        if (room.classList.contains("open")) {
+            room.classList.remove("open");
+        } else {
+            room.classList.add("open");
+        }
+    })
+}
+
+// 열림 버튼 클릭 시
+for (let btnOpen of btnOpens) {
+    btnOpen.addEventListener("click", function () {
+        
+    })
+}
+
+
+// 세계로 클릭 시
+segyero.addEventListener("click", function () {
+
+})
