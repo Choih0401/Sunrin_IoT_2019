18587d76aa11e9655d83ce7af13f2a5da90e205c
Party4Bread <solsam10@naver.com>
2019-09-16 17:51:51 +0900

    servrer

--- routers/api.js
+++ routers/api.js
@@ -2,20 +2,38 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const db = require('../db/connector.js');
+const Room = require('../db/rooms')
 
-router.get("/rentplan",function(req,res){
+router.get("/rentplan", function(req, res) {
     //req.body.  
     const id = req.query.username
     const selectQuery = "SELECT  (SELECT class FROM user where id = ? LIMIT 1)";
-    db.query(selectQuery, [id, passwd], function(err, result){
-        
+    db.query(selectQuery, [id, passwd], function(err, result) {
+
     })
 })
 
-router.post("/newotp"function(req,res){
-    const id = req.query.username
-    const selectQuery = "UPDATE class FROM user where id = ? LIMIT 1)";
-    db.query(selectQuery, [id, passwd], function(err, result){
-        
-    })
+router.get("/newotp", function(req, res) {
+    let no = req.query.no;
+    let newotp = req.query.value;
+    console.log(no)
+    console.log(newotp)
+    Room.findOne({ where: { no: no } }).then(
+        room => {
+            if (!room) {
+                return Room.create({ no: no })
+            }
+            return room
+        }
+    ).then(
+        room => {
+            return Room.update({ otp: newotp }, { where: { id: room.id } })
+        }
+    ).then(
+        r => {
+            res.status(200).send("GOOOOD")
+        }
+    ).catch(reason => res.status(500).send(reason))
 })
+
+module.exports = router
